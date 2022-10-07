d5ffdf5d25499cbb2281b8f613aa69623f154dd9
Nicolás Prieto <nicolas@prieto.in>
2022-10-07 19:18:57 -0300

    Genera variable global constante para ancho de botones en grid.

--- MyDailyCoffee2/Constants.cs
+++ MyDailyCoffee2/Constants.cs
@@ -3,5 +3,9 @@
     public static class Constants
     {
         public static string PhoneNumberRegex => @"^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$";
+
+        public static string DualMiniButtonPx => "95px";
+
+        public static string SingleMiniButtonPx => "60px";
     }
 }
