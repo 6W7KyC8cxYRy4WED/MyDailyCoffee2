13acbaad31641ead500bde72148b686078232b84
Nicolás Prieto <nicolas@prieto.in>
2022-10-16 11:53:49 -0300

    Crea proyecto de consola para poder ejecutar el vaciado de información de la base de datos.

---
+++ EmptyDatabaseScript/Program.cs
@@ -0,0 +1,12 @@
+﻿using MyDailyCoffee2.Model;
+
+namespace EmptyDatabaseScript
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Script.Run();
+        }
+    }
+}
---
+++ EmptyDatabaseScript/Script.cs
@@ -0,0 +1,25 @@
+﻿using MyDailyCoffee2.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmptyDatabaseScript
+{
+    public static class Script
+    {
+        public static void Run()
+        {
+            DatabaseContext databaseContext = new DatabaseContext();
+            databaseContext.RemoveRange(databaseContext.CustomerPhoneNumbers);
+            databaseContext.SaveChanges();
+            databaseContext.RemoveRange(databaseContext.Customers);
+            databaseContext.SaveChanges();
+            databaseContext.RemoveRange(databaseContext.AzureUsers);
+            databaseContext.SaveChanges();
+            databaseContext.RemoveRange(databaseContext.MaterialBrands);
+            databaseContext.SaveChanges();
+        }
+    }
+}
