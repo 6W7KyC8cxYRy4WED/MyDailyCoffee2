d7d6254c92b2deb93e887824ee4af1efdb53d2bf
Nicolás Prieto <nicolas@prieto.in>
2022-10-16 12:05:08 -0300

    Crea proyecto de consola para poder ejecutar el llenado de información de la base de datos.

---
+++ PopulateDatabaseScript/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PopulateDatabaseScript
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Script.Run();
+        }
+    }
+}
--- PopulateDatabaseScript/Script.cs
+++ PopulateDatabaseScript/Script.cs
@@ -1,40 +1,41 @@
 ﻿using CsvHelper;
 using CsvHelper.Configuration;
-using Microsoft.EntityFrameworkCore;
 using MyDailyCoffee2.Model;
+using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
 using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace MyDailyCoffee2.TestData
+namespace PopulateDatabaseScript
 {
-    public static class DatabasePopulator
+    public static class Script
     {
-        public static void Populate()
+        public static void Run()
         {
+            EmptyDatabaseScript.Script.Run();
             DatabaseContext databaseContext = new DatabaseContext();
-            databaseContext.RemoveRange(databaseContext.CustomerPhoneNumbers);
-            databaseContext.RemoveRange(databaseContext.Customers);
-            databaseContext.RemoveRange(databaseContext.AzureUsers);
-            databaseContext.SaveChanges();
             CsvConfiguration csvConfiguration = new CsvConfiguration(CultureInfo.InvariantCulture);
             csvConfiguration.HeaderValidated = null;
             csvConfiguration.MissingFieldFound = null;
             csvConfiguration.Delimiter = ";";
-            StreamReader streamReader = new StreamReader(@"TestData\AzureUsers.csv");
+            StreamReader streamReader = new StreamReader("AzureUsers.csv");
             CsvReader csvReader = new CsvReader(streamReader, csvConfiguration);
             List<AzureUser> azureUsers = csvReader.GetRecords<AzureUser>().ToList();
             databaseContext.AddRange(azureUsers);
             databaseContext.SaveChanges();
-            streamReader = new StreamReader(@"TestData\Customers.csv");
+            streamReader = new StreamReader("Customers.csv");
             csvReader = new CsvReader(streamReader, csvConfiguration);
             List<Customer> customers = csvReader.GetRecords<Customer>().ToList();
             azureUsers = databaseContext.AzureUsers.ToList();
             customers.ForEach(c => c.CreatedBy = c.UpdatedBy = azureUsers.First());
-
-            foreach (Customer customer in customers)
-            {
-                databaseContext.Add(customer);
-            }
-
+            databaseContext.AddRange(customers);
+            streamReader = new StreamReader("MaterialBrands.csv");
+            csvReader = new CsvReader(streamReader, csvConfiguration);
+            List<MaterialBrand> materialBrands = csvReader.GetRecords<MaterialBrand>().ToList();
+            databaseContext.MaterialBrands.AddRange(materialBrands);
             databaseContext.SaveChanges();
         }
     }
